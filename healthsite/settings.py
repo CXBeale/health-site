854f0ef04d41a0aa174f74ae7e27e999ca85a86f
Christian <cxbeale@gmail.com>
2025-10-31 12:03:19 +0000

    add requirements.txt, add env file for secret key and env vars

--- healthsite/settings.py
+++ healthsite/settings.py
@@ -11,22 +11,24 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-03z%dn)+2z#24fh^93t_tc5f2!d7bx-jrqrmvh_%#8_oa$%ibu'
-
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "dev-unsafe-fallback")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+DEBUG = os.getenv("DJANGO_DEBUG", "True").lower() == "true"
 
+allowed_hosts = os.getenv("DJANGO_ALLOWED_HOSTS", "")
+ALLOWED_HOSTS = [h.strip() for h in allowed_hosts.split(",")] if allowed_hosts else []
 
 # Application definition
 
